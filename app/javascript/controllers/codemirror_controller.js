8b0c1935fece4dde80e94b30d14652e53f85c23f
osadasami <osadasami@gmail.com>
2022-03-26 16:38:01 +0500

    load all modes for codemirror

--- app/javascript/controllers/codemirror_controller.js
+++ app/javascript/controllers/codemirror_controller.js
@@ -1,5 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 import CodeMirror from 'codemirror'
+import 'codemirror/mode/gfm/gfm'
 
 // Connects to data-controller="codemirror"
 export default class extends Controller {
@@ -7,10 +8,22 @@ export default class extends Controller {
     const editor = CodeMirror.fromTextArea(this.element, {
       tabSize: 2,
       indentWithTabs: false,
+      theme: 'default',
+      mode: {
+        name: 'gfm',
+        highlightFormatting: true,
+      },
     })
 
     editor.on('blur', () => {
       this.dispatch('blur', {target: document})
     })
+
+    editor.on('change', (editor) => {
+      console.log(editor.getValue())
+    })
   }
 }
+
+
+
