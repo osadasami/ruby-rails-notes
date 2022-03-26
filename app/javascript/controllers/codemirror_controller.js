01eb5263b7c84f5a2c01649fb7cf5b77baa448ad
osadasami <osadasami@gmail.com>
2022-03-26 13:06:02 +0500

    fix codemirror tabsize

--- app/javascript/controllers/codemirror_controller.js
+++ app/javascript/controllers/codemirror_controller.js
@@ -4,7 +4,10 @@ import CodeMirror from 'codemirror'
 // Connects to data-controller="codemirror"
 export default class extends Controller {
   connect() {
-    const editor = CodeMirror.fromTextArea(this.element)
+    const editor = CodeMirror.fromTextArea(this.element, {
+      tabSize: 2,
+      indentWithTabs: false,
+    })
 
     editor.on('blur', () => {
       this.dispatch('blur', {target: document})
