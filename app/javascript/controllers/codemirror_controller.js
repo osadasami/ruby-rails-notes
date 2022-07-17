ad8bd84b015e081e553e4d3116109e6caf86bf83
osadasami <osadasami@gmail.com>
2022-07-17 08:41:20 +0500

    update

--- app/javascript/controllers/codemirror_controller.js
+++ app/javascript/controllers/codemirror_controller.js
@@ -1,6 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 import CodeMirror from 'codemirror'
 import 'codemirror/mode/gfm/gfm'
+import 'codemirror/addon/hint/show-hint'
 
 // Connects to data-controller="codemirror"
 export default class extends Controller {
@@ -21,13 +22,42 @@ export default class extends Controller {
       })
     })
 
-    editor.on('change', (editor) => {
+    editor.on('change', () => {
       this.dispatch('change', {
         target: document,
         detail: {
           content: editor.getValue()
         }
       })
+    })
+
+    editor.on('keydown', (_, event) => {
+      if(event.key === '#') {
+        const snippets = [
+          { text: 'const', displayText: 'const declarations' },
+          { text: 'let', displayText: 'let declarations' },
+          { text: 'var', displayText: 'var declarations' },
+        ]
+
+        CodeMirror.showHint(editor, () => {
+          const cursor = editor.getCursor()
+          const token = editor.getTokenAt(cursor)
+          const start = token.start
+          const end = cursor.ch
+          const line = cursor.line
+          const currentWord = token.string
+
+          const list = snippets.filter(function (item) {
+            return item.text.indexOf(currentWord) >= 0
+          })
+
+          return {
+            list: list.length ? list : snippets,
+            from: CodeMirror.Pos(line, start),
+            to: CodeMirror.Pos(line, end)
+          }
+        }, { completeSingle: false })
+      }
     })
   }
 }
